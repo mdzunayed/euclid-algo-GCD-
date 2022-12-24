780660e42ac0d7c42fd6e267b1ff909b68ba908b
Md Zunayed <120125367+mdzunayed@users.noreply.github.com>
2022-12-25 00:11:20 +0600

    Add files via upload

---
+++ euclid_algorithm_gcd.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int gcd(int x,int y)
+{
+    if(y==0) return x;
+    return gcd(y,x%y);
+}
+
+int main()
+{
+    int testcase; cin>>testcase;
+    while (testcase--)
+    {
+        int x,y; cin>>x>>y;
+        cout<<gcd(x,y)<<endl;
+    }
+    
+}
